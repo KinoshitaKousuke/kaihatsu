5493e51e38dc025234fdda7b0e5186b494f48341
pohon <7koika0214@gmail.com>
2024-05-29 21:19:26 +0900

    update

--- index.js
+++
@@ -1,7 +0,0 @@
-async function callApi() {
-    const res = await fetch("https://jsonplaceholder.typicode.com/users");
-    const users = await res.json();
-    console.log(users)
-  };
-  
-  callApi();
